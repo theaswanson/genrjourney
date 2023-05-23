0766cbc975d04bc974da2a9daecbf0dfb2043d11
Adam Swanson <adam.swanson.98@gmail.com>
2023-05-22 22:08:32 -0400

    Initialize Google Analytics

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ReactGA from "react-ga4";
 import App from "./App";
 
 import "./index.css";
@@ -9,6 +10,10 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+
+ReactGA.initialize("G-0S9MPWB6LC", {
+  testMode: process.env.NODE_ENV === "development",
+});
 
 const router = createBrowserRouter([
   {
