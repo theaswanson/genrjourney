d213a6efffcb5a40d7bfe1b5ffd8c0a2dcaa61ba
Adam Swanson <adam.swanson.98@gmail.com>
2023-05-22 21:52:00 -0400

    Formatting

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,38 @@ import { Comment } from "./Comment";
 import "./App.css";
 
 const App = () => {
-	const quotes = [
-		{ quote: "\"You have hit a topic that needs addressing in our culture and churches today.\"", author: "Pastor K. - Fort Collins, CO" },
-		{ quote: "\"This is an answer to prayer. I can make many sermons from this.\"", author: "Pastor A. - Danville, IL" },
-		{ quote: "\"This is what I have been waiting for.\"", author: "Pastor D. - Danville, IL" },
-		{ quote: "\"You are leaving a legacy.\"", author: "Bible study member - Danville, IL" },
-		{ quote: "\"The timing is perfect. I am starting a discipleship program.\"", author: "Pastor H. - Danville, IL" },
-		{ quote: "\"This book can be used for teaching.\"", author: "Pastor B. - Danville, IL" },
-		{ quote: "\"Can I make copies for each of my staff members?\"", author: "Pastor S. - Danville, IL" },
-	];
+  const quotes = [
+    {
+      quote:
+        '"You have hit a topic that needs addressing in our culture and churches today."',
+      author: "Pastor K. - Fort Collins, CO",
+    },
+    {
+      quote:
+        '"This is an answer to prayer. I can make many sermons from this."',
+      author: "Pastor A. - Danville, IL",
+    },
+    {
+      quote: '"This is what I have been waiting for."',
+      author: "Pastor D. - Danville, IL",
+    },
+    {
+      quote: '"You are leaving a legacy."',
+      author: "Bible study member - Danville, IL",
+    },
+    {
+      quote: '"The timing is perfect. I am starting a discipleship program."',
+      author: "Pastor H. - Danville, IL",
+    },
+    {
+      quote: '"This book can be used for teaching."',
+      author: "Pastor B. - Danville, IL",
+    },
+    {
+      quote: '"Can I make copies for each of my staff members?"',
+      author: "Pastor S. - Danville, IL",
+    },
+  ];
 
   return (
     <>
@@ -44,7 +67,7 @@ const App = () => {
           </Typography>
           <Button
             variant="contained"
-						size="large"
+            size="large"
             href={process.env.PUBLIC_URL + "/Stop Murmuring.pdf"}
             target="_blank"
             sx={{ mb: 8 }}
@@ -53,12 +76,14 @@ const App = () => {
           </Button>
 
           <Box>
-						{quotes.map((q) => <Comment quote={q.quote} author={q.author} />)}
+            {quotes.map((q) => (
+              <Comment quote={q.quote} author={q.author} />
+            ))}
           </Box>
         </Grid>
       </div>
     </>
   );
-}
+};
 
 export default App;
